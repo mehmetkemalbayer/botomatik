5fcdb30ca52fabc35ef8e0caaaa10d5fd9842add
mehmetkemalbayer <kemal.bayer@gmail.com>
2017-04-10 00:32:21 +0300

    first commit

---
+++ app/controllers/hom_controller.rb
@@ -0,0 +1,2 @@
+class HomController < ApplicationController
+end
---
+++ test/controllers/hom_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class HomControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
